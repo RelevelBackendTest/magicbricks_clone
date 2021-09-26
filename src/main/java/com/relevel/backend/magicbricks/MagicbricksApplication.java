27afe9e51247d5bc82d3d221c9160b6ed93d0452
RelevelBackendTest <87223414+RelevelBackendTest@users.noreply.github.com>
2021-09-27 00:05:35 +0530

    Add files via upload

---
+++ src/main/java/com/relevel/backend/magicbricks/MagicbricksApplication.java
@@ -0,0 +1,13 @@
+package com.relevel.backend.magicbricks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicbricksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicbricksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/relevel/backend/magicbricks/MagicbricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relevel.backend.magicbricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicbricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
